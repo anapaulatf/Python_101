c209d4b7ba88895e9d14ca0faf663733b822e003
ANAPAULATF <aptavaresdafonseca@gmail.com>
2021-02-10 11:29:04 -0300

    Atualizacao 10 de fevereiro de 2021

---
+++ WorldGapminder.py
@@ -0,0 +1,28 @@
+
+# gapminder DATA
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from gapminder import gapminder
+gapminder.head()
+
+
+pd_gapminder = pd.DataFrame(gapminder)
+type(pd_gapminder["country"])
+help(pd.DataFrame)
+
+pd_gapminder[['country']]
+pd_gapminder[["country", "year", "pop"]]
+albania = pd_gapminder.loc[12:23]
+
+
+df = pd_gapminder.set_index('country')
+df.head()
+df_albania = df.loc["Albania"]
+df_albania.head()
+
+albania_pop = df.loc[["Albania"],["year","pop"]]
+
+albania_pop = albania_pop.set_index("year")
+plt.plot(albania_pop)
---
+++ datacamp.py
@@ -0,0 +1,64 @@
+# MATPLOTLIB
+
+import matplotlib.pyplot as plt
+year = [1950, 1970, 1990, 2010]
+pop = [2.519, 3.692, 5.263, 6.972]
+plt.plot(year, pop)
+plt.show()
+
+plt.scatter(year, pop)
+plt.show()
+
+
+
+# histogram
+values = [0, 0.6, 1.4, 1.6, 2.2, 2.5, 2.6, 3.2, 3.5, 3.9, 4.2, 6]
+plt.hist(values, bins=3)
+plt.show()
+
+
+# customizations
+plt.plot(year, pop)
+plt.xlabel('Year')
+plt.ylabel('Population')
+plt.title('População Mundial')
+plt.yticks([0,2,4,6,8,10],
+['0', '2B', '4B', '6B', '8B', '10B'])
+plt.show()
+
+
+#dictionaries
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe['italy'] = 'rome'
+
+# Print out italy in europe
+print('italy' in europe)
+
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Print europe
+print(europe)
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = {'capital':'rome', 'population':59.83}
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+# Print europe
+print(europe)
